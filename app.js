e52197f32093ab201fab7cc647765de4fb7d5d3e
Irving Viveros <irvingviveros@outlook.com>
2020-10-30 11:49:44 -0600

    HTML, CSS and JS added

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const bodyParser = require("body-parser");
 const https = require("https");
 
 const app = express();
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.listen(3000, function(){
-    console.log("Server is running on port 3000");
-})
+app.listen(3000, function () {
+  console.log("Server is running on port 3000");
+});
