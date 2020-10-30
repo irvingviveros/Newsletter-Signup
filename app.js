0ec58f8486ff92ee195ba15faae05b0c395eda30
Irving Viveros <irvingviveros@outlook.com>
2020-10-30 15:14:44 -0600

    Initial express routes updated

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
+//jshint esversion: 6
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const https = require("https");
+const { dirname } = require("path");
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/signup.html");
+});
 
 app.listen(3000, function () {
   console.log("Server is running on port 3000");
