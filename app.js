9dbe14e1bd1cda0df22dece17d3e35c221e3f44f
Irving Viveros <irvingviveros@outlook.com>
2020-10-30 15:39:24 -0600

    Request added

--- app.js
+++ app.js
@@ -13,6 +13,14 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + "/signup.html");
 });
 
+app.post("/", function (req, res) {
+  const firstName = req.body.firstName;
+  const lastName = req.body.lastName;
+  const userEmail = req.body.email;
+
+  console.log(firstName + " " + lastName + " " + userEmail);
+});
+
 app.listen(3000, function () {
   console.log("Server is running on port 3000");
 });
